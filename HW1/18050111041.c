61823896c801365e41e89595a2e62fb9863fa1a9
arkSchwarz <batuhanordek@hotmail.com>
2023-03-20 23:35:13 +0300

    Added HW1 again move HW2 to the file

--- HW1/18050111041.c
+++ HW1/18050111041.c
@@ -1,53 +1,8 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "hellomake.h"
 
-int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        printf("Usage: %s rows columns output_file\n", argv[0]);
-        return 1;
-    }
+int main() {
+  // call a function in another file
+  myPrintHelloMake();
 
-    int rows = atoi(argv[1]);
-    int columns = atoi(argv[2]);
-    char *output = argv[3];
-
-    double **matrix = (double **) malloc(rows * sizeof(double *));
-    for (int i = 0; i < rows; i++) {
-        matrix[i] = (double *) calloc(columns, sizeof(double));
-    }
-
-   long int i = 18050111041;
-    srand(i);
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            matrix[i][j] = (double) rand() / RAND_MAX * 99.0 + 1.0;
-        }
-    }
-
-    double *vector = (double *) calloc(columns, sizeof(double));
-    for (int i = 0; i < columns; i++) {
-        vector[i] = (double) rand() / RAND_MAX * 99.0 + 1.0;
-    }
-
-    double *result = (double *) calloc(rows, sizeof(double));
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            result[i] += matrix[i][j] * vector[j];
-        }
-    }
-
-    FILE *file = fopen(output, "w");
-    for (int i = 0; i < rows; i++) {
-        fprintf(file, "%lf\n", result[i]);
-    }
-    fclose(file);
-
-    free(matrix);
-    free(vector);
-    free(result);
-
-    return 0;
+  return(0);
 }
-
-
